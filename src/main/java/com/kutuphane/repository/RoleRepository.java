d87916bd18602bded76cf468f311685054368c19
Muhammet Enes AKÇAYIR <muhammetenesakcayir@gmail.com>
2020-07-11 23:37:05 +0300

    Update

--- src/main/java/com/kutuphane/repository/RoleRepository.java
+++ src/main/java/com/kutuphane/repository/RoleRepository.java
@@ -4,5 +4,4 @@ import com.kutuphane.model.type.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, String> {
-    public Role findByRole(String role);
 }
